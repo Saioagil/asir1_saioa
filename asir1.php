a9d8d1fe804b44c9bfa752c0faa45843ac69ca1b
Saioagil <saioagilr1@gmail.com>
2019-09-30 10:47:53 +0200

    fecha y hora

--- asir1.php
+++ asir1.php
@@ -4,6 +4,8 @@
 <a hred="http://192.168.0.98/asir1_kevin/asir1.php">kevin</a>
 <?php
 $f=fopen('visitas.txt','a');
-fwrite($f,$_SERVER['REMOTE_ADDR']."\r\n");
+fwrite($f,$_SERVER['REMOTE_ADDR']);
+fwrite($f,date (' Y-m-d H:i'));
+fwrite($f,"\r\n");
 fclose($f)
-?>$datos="La ip es:".$ip." y fecha: ".$hoy."\r\n";
+?>
