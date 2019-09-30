6387ea90512a89bd962f14ec1351dd6194aefaf9
Saioagil <saioagilr1@gmail.com>
2019-09-30 12:51:14 +0200

    funciona

--- 2b.php
+++ 2b.php
@@ -1,20 +1,26 @@
 <?php
+
 $a=50;
 $b=25;
-
+function calcula($a,$b){
 echo("Suma de $a y $b= ");
 echo($a+$b);
 echo'<br/>';
 echo'<br/>';
+
 echo("Resta de $a y $b= ");
 echo($a-$b);
 echo'<br/>';
 echo'<br/>';
+
 echo("Cociente de $a y $b= ");
 echo($a/$b);
 echo'<br/>';
 echo'<br/>';
+
 echo("Producto de $a y $b= ");
-echo($a*$b)
+echo($a*$b);
+
+}calcula($a,$b);
 
 ?>
