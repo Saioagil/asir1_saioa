01d84e3b3b15364a101bdc12bd0fb8d38cbf508d
Saioagil <saioagilr1@gmail.com>
2019-09-30 10:52:27 +0200

    fecha y hora

--- asir1.php
+++ asir1.php
@@ -1,7 +1,7 @@
 <a>saioa</a>
 </br>
 <a href="http://192.168.0.89/asir1_ruben/asir1.php">ruben</a>
-<a hred="http://192.168.0.98/asir1_kevin/asir1.php">kevin</a>
+<a href="http://192.168.0.98/asir1_kevin/asir1.php">kevin</a>
 <?php
 $f=fopen('visitas.txt','a');
 fwrite($f,$_SERVER['REMOTE_ADDR']);
