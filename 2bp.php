66bef2e591e02f564b623e4f287b4e7ec9a9336d
Saioagil <saioagilr1@gmail.com>
2019-10-07 10:15:48 +0200

    funciona

---
+++ 2bp.php
@@ -0,0 +1,11 @@
+<?php
+function calcula($a,$b){
+	
+echo "Suma: " .$a."+".$b."= ".($a+$b). '</br>';
+echo "Resta: " .$a."-".$b."= ".($a-$b). '</br>';
+echo "División: " .$a."/".$b."= ".($a/$b). '</br>';
+echo "Multiplicación: " .$a."x".$b."= ".($a*$b). '</br>';
+
+}calcula(50,25);
+calcula(480,10);
+?>
